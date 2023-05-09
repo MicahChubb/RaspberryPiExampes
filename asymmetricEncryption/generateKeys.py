0ad63645d096fff04a60035fe9529c6f66b120f6
Micah Chubb <89553508+MicahChubb@users.noreply.github.com>
2023-05-10 09:59:18 +1000

    Update generateKeys.py

--- asymmetricEncryption/generateKeys.py
+++ asymmetricEncryption/generateKeys.py
@@ -1,15 +1,19 @@
 from ecies.utils import generate_key
 import binascii
 
-# Create the key pair, store it into variables
-eth_k = generate_key()
-sk_bytes = eth_k.secret  # hex string
-pk_bytes = eth_k.public_key.format(True)  # hex string
+def makeKeys():
+  # Create the key pair, store it into variables
+  eth_k = generate_key()
+  sk_bytes = eth_k.secret  # hex string
+  pk_bytes = eth_k.public_key.format(True)  # hex string
 
-# Encode the byte to a hex value and write out to different files
-with open('secret.txt', 'wb') as f:
+  # Encode the byte to a hex value and write out to different files
+  with open('textfiles/secret.txt', 'wb') as f:
     f.write(binascii.hexlify(sk_bytes))
-with open('public.txt', 'wb') as f:
-  f.write(binascii.hexlify(pk_bytes))
+  with open('textfiles/public.txt', 'wb') as f:
+    f.write(binascii.hexlify(pk_bytes))
 
-print("generated keys!")
+  print("generated keys!")
+
+if __name__ == "__main__":
+  makeKeys()
