f25dde989118c3cc7d7e9eb597fefc1a0e2136ce
Micah Chubb <89553508+MicahChubb@users.noreply.github.com>
2023-05-10 09:59:53 +1000

    Update encrypt.py

--- asymmetricEncryption/encrypt.py
+++ asymmetricEncryption/encrypt.py
@@ -1,17 +1,18 @@
 from ecies import encrypt
 import binascii
 
-# Read in from our text file, convert from hex to a byte
-with open('public.txt') as f:
-    for line in f:
-      pk_hex = binascii.unhexlify(''.join(line.split()))
+def encryptMessage(data):
+    # Read in from our text file, convert from hex to a byte
+    with open('textfiles/public.txt') as f:
+        for line in f:
+            pk_hex = binascii.unhexlify(''.join(line.split()))
 
-# Test string:
-data = b'this is a test'
+    # Encrypt the test string and then write out to a file as hex
+    encryptedMessage = encrypt(pk_hex, data.encode("utf8"))
+    with open('textfiles/encrypted.txt', 'wb') as f:
+        f.write(binascii.hexlify(encryptedMessage))
+    
+    print("encrypted message!")
 
-# Encrypt the test string and then write out to a file as hex
-encryptedMessage = encrypt(pk_hex, data)
-with open('encrypted.txt', 'wb') as f:
-    f.write(binascii.hexlify(encryptedMessage))
-  
-print("encrypted message!")
+if __name__ == "__main__":
+  encryptMessage('this is some test data')
