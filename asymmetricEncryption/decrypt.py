879cded19aed2dcba184422231b4101de09f5eee
Micah Chubb <89553508+MicahChubb@users.noreply.github.com>
2023-05-10 10:00:42 +1000

    Update decrypt.py

--- asymmetricEncryption/decrypt.py
+++ asymmetricEncryption/decrypt.py
@@ -1,16 +1,20 @@
 from ecies import decrypt
 import binascii
 
-# Read in and un-hex our private key
-with open('secret.txt') as f:
+def decryptMessage():
+  # Read in and un-hex our private key
+  with open('textfiles/secret.txt') as f:
     for line in f:
       sk_bytes = binascii.unhexlify(''.join(line.split()))
 
-# Read in and un-hex our message
-with open('encrypted.txt') as f:
+  # Read in and un-hex our message
+  with open('textfiles/encrypted.txt') as f:
     for line in f:
       data = binascii.unhexlify(''.join(line.split()))
 
-# Decrypt our message and write it out!
-decryptedMessage = decrypt(sk_bytes, data)
-print("Decrypted:", decryptedMessage)
+  # Decrypt our message and write it out!
+  decryptedMessage = decrypt(sk_bytes, data)
+  print("Decrypted:", decryptedMessage.decode("utf8")) #The decode is to remove a b'text here' formatting
+
+if __name__ == "__main__":
+  decryptMessage()
